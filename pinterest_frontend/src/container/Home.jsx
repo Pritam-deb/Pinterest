a12ad2d44546d4ef5e36b8f54dd8875b5872365c
Pritam <pritamdebnath297@gmail.com>
2022-01-05 00:07:03 +0530

    feat: pulled user data from sanity to display in home Sidebar

--- pinterest_frontend/src/container/Home.jsx
+++ pinterest_frontend/src/container/Home.jsx
@@ -3,13 +3,45 @@ import { HiMenu } from "react-icons/hi";
 import { Link, Route, Routes } from "react-router-dom";
 import { AiFillCloseCircle } from "react-icons/ai";
 
+import { userQuery } from "../utils/data";
 import { Sidebar, UserProfile } from "../components";
 import { client } from "../client";
 import logo from "../assets/logo.png";
 import Pins from "./Pins";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+  return (
+    <div className="flex bg-grey-50 md:flex-row flex-col h-screen">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(false)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="logo" className="w-28" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
