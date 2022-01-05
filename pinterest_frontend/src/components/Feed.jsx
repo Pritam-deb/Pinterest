48c73541b72aa92067bc1fef391bef8eeba1a407
Pritam <pritamdebnath297@gmail.com>
2022-01-05 21:34:54 +0530

    feat: added a loading state

--- pinterest_frontend/src/components/Feed.jsx
+++ pinterest_frontend/src/components/Feed.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+    } else {
+    }
+  }, [categoryId]);
+  if (loading) return <Spinner message="we are adding new ideas to you feed" />;
   return <div>Feed</div>;
 };
 
