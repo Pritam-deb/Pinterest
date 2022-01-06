75da2cfdad0452fbfb01804f218bdd9888cb545f
Pritam <pritamdebnath297@gmail.com>
2022-01-07 00:48:36 +0530

    feat: added multiple fields to upload image
    title, about image, destination link, category from a list and button to save pin

--- pinterest_frontend/src/components/CreatePin.jsx
+++ pinterest_frontend/src/components/CreatePin.jsx
@@ -47,6 +47,39 @@ const CreatePin = ({ user }) => {
     }
   };
 
+  const savePin = () => {
+    if (title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: "pin",
+        title,
+        about,
+        destination,
+        image: {
+          _type: "image",
+          asset: {
+            _type: "reference",
+            _ref: imageAsset?._id,
+          },
+        },
+        userId: user._id,
+        postedBy: {
+          _type: "postedBy",
+          _ref: user._id,
+        },
+        category,
+      };
+      client.create(doc).then(() => {
+        navigate("/");
+      });
+    } else {
+      setFields(true);
+
+      setTimeout(() => {
+        setFields(false);
+      }, 2000);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
       {fields && (
@@ -69,8 +102,7 @@ const CreatePin = ({ user }) => {
                     <p className="text-lg">Click to upload</p>
                   </div>
                   <p className="mt-32 text-gray-400">
-                    Reccomendation: use high-quality JPG, SVG, PNG, GIF or TIFF
-                    less than 20MB
+                    Use high-quality JPG, SVG, GIF or TIFF less than 20MB
                   </p>
                 </div>
                 <input
@@ -96,6 +128,73 @@ const CreatePin = ({ user }) => {
                 </button>
               </div>
             )}
+          </div>
+        </div>
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add your title"
+            className="outline-none text-xl sm:text-xl font-semibold border-b-2 border-gray-200 p-2"
+          />
+          {user && (
+            <div className="flex gap-2 mt-2 mb-2 items-center bg-white rounded-lg">
+              <img
+                src={user.image}
+                className="w-10 h-10 rounded-full"
+                alt="user-profile"
+              />
+              <p className="font-bold">{user.userName}</p>
+            </div>
+          )}
+          <input
+            type="text"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="Tell everyone what your Pin is about"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <input
+            type="url"
+            vlaue={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Add a destination link"
+            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
+          />
+          <div className="flex flex-col">
+            <div>
+              <p className="mb-2 font-semibold text:lg sm:text-xl">
+                Choose Pin Category
+              </p>
+              <select
+                onChange={(e) => {
+                  setCategory(e.target.value);
+                }}
+                className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+              >
+                <option value="others" className="sm:text-bg bg-white">
+                  Select Category
+                </option>
+                {categories.map((item) => (
+                  <option
+                    className="text-base border-0 outline-none capitalize bg-white text-black "
+                    value={item.name}
+                  >
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex justify-end items-end mt-5">
+              <button
+                type="button"
+                onClick={savePin}
+                className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
+              >
+                Save Pin
+              </button>
+            </div>
           </div>
         </div>
       </div>
